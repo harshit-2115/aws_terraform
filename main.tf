f458b5804c43cb1135efe45f3c18662808e7e41f
Harshit <harshit.2115@gmail.com>
2020-02-23 01:45:50 +0530

    deployed a server

--- main.tf
+++ main.tf
@@ -5,8 +5,23 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-0d9462a653c34dab7"
     instance_type = "t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+    user_data = file("file.sh")
+
 
     tags = {
         Name = "terraform-example"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
